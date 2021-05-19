62b316d5f1af444282dd1878cca5bad3ba557ca3
mingabire809 <michelingabire190508@daystar.ac.ke>
2021-05-19 13:41:07 +0300

    second

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -19,12 +19,19 @@ class MainActivity : AppCompatActivity() {
         btnSendMsgToNextActivity.setOnClickListener {
 
             val message: String = etUserMessage.text.toString()
-            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-
-            val intent = Intent(this,SecondActivity::class.java)
+            val intent = Intent(this,SecondActivity::class.java)  //explicit intent
+            intent.putExtra("user_message",message)
             startActivity(intent)
         }
         
+        btnsharetootherapps.setOnClickListener {
+            val message: String = etUserMessage.text.toString()
+            val intent = Intent()
+            intent.action = Intent.ACTION_SEND
+            intent.putExtra(Intent.EXTRA_TEXT,message)
+            intent.type = "text/plain"
+            startActivity(Intent.createChooser(intent, "Share to: "))
+        }
 
 
 
